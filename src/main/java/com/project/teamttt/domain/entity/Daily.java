de33899db6931f801414c618f4199596c5ad16a4
KCH <130543414+kkkkcccchhhh@users.noreply.github.com>
2024-03-19 20:38:33 +0900

    project setting v2

--- src/main/java/com/project/teamttt/domain/entity/Daily.java
+++ src/main/java/com/project/teamttt/domain/entity/Daily.java
@@ -1,4 +1,44 @@
 package com.project.teamttt.domain.entity;
 
-public class Daily {
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "DAILY")
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Daily extends BaseEntity{
+    /**
+     * 데일리 아이디
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long dailyId;
+
+    /**
+     * 데일리 제목
+     */
+    private String title;
+
+    /**
+     * 데일리 내용
+     */
+    private String content;
+
+    /**
+     * 데일리 공개 여부
+     */
+    private Boolean isPublic;
+
+    /**
+     * member 테이블에 member_id 컬럼을 참조
+     */
+    @ManyToOne
+    @JoinColumn(name = "member_id", referencedColumnName = "member_id")
+    private Member member;
 }
--- src/main/java/com/project/teamttt/domain/entity/Member.java
+++ src/main/java/com/project/teamttt/domain/entity/Member.java
@@ -1,4 +1,51 @@
 package com.project.teamttt.domain.entity;
 
-public class Member {
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "Members")
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+
+public class Member extends BaseEntity{
+
+    /**
+     * 맴버 아이디 (PK)
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    /**
+     * 맴버 이메일
+     */
+    private String email;
+
+    /**
+     * 맴버 패스워드
+     */
+    private String password;
+
+    /**
+     * 맴버 닉네임
+     */
+    private String nickname;
+
+    /**
+     * 소셜 이름
+     */
+    private String social;
 }
