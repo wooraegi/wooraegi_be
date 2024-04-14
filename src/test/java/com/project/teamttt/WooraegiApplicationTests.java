a7667c5e88a40b66c962df2c78392c3d3b682239
김시연 <kimsiyeon@MacBook-Pro-2.local>
2024-04-14 16:39:47 +0900

    junit 추가

---
+++ src/test/java/com/project/teamttt/WooraegiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.teamttt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooraegiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
